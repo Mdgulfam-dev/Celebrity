02125bd20e3a3295c3e3307f9e294e4d0824dcd3
Md Gulfam <mdgulfam0505@gmail.com>
2025-06-30 11:23:48 +0530

    old code

--- backend/config/db.js
+++ backend/config/db.js
@@ -23,8 +23,8 @@ export const connectDB = async () =>{
   await retry(async () => {
     try {
       await mongoose.connect(process.env.DB_URL, {
-        // useNewUrlParser: true,
-        // useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
         timeoutMS: 30000 // 30 seconds
       });
       console.log("DB connected");
--- backend/models/RecommendProfileModel.js
+++ backend/models/RecommendProfileModel.js
@@ -1,36 +1,4 @@
 import mongoose from "mongoose";
-
-
-
-// const RecommendProfileSchema = new mongoose.Schema(
-//   {
-//     name: { type: String, required: true, unique: true },
-//     age: { type: Number, required: true },
-//     location: { type: String, required: true },
-//     gender: { type: String, required: true },
-//     targetAudience: String,
-//     brandAlignment: String,
-//     imageMatch: String,
-//     contentStyle: String,
-//     publicImage: String,
-//     followers: { type: Number, required: true }, // Ensure Number type
-//     engagementRate: String, //  Use fanEngagementRate from JSON
-//     totalReach: String,
-//     previousEndorsements: String,
-//     averageROI: String,
-//     pastPartnerships: String,
-//     endorsementFee: String,
-//     geographicLocation: String,
-//   },
-//   { collection: "recommendProfiles" } //  Correct collection name
-// );
-
-
-
-// export default mongoose.model("RecommendProfile", RecommendProfileSchema);
-
-
-
 const RecommendProfileSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, unique: true },
